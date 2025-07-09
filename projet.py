7706020793b1e89df0c7b29e2a7135aa9fa042f6
Ben Abbes Islam <benabbesislam0@gmail.com>
2025-07-09 20:13:16 +0100

    Fusion squash de projetb2

--- projet.py
+++ projet.py
@@ -1,4 +1,8 @@
-<<<<<<< HEAD
+Data = [1, 3, 5]
+print('le produit est :', produit(Data))
+print('le min est :', min(Data))
+print('le max est :', max(Data))
+
 Data = [1, 3, 5]
 if Data :
 print('le produit est :', produit(Data))
@@ -6,5 +10,4 @@ print('le min est :', min(Data))
 print('le max est :', max(Data))
 else :
 print ('dossier vide')
->>>>>>> projetb2
 
