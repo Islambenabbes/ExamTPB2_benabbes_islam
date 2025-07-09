fd162c21f92ca170879bed362d739712fb520cfa
Ben Abbes Islam <benabbesislam0@gmail.com>
2025-07-09 20:09:32 +0100

    Fusion squash de projetb2

--- projet.py
+++ projet.py
@@ -1,18 +1,4 @@
 <<<<<<< HEAD
-def produit(T):
-    S = 1
-    for t in T:
-        S *= t
-    return S
-=======
-import math
-
-Data=[1,3,5]
-Prod = math.prod(Data)
-print ('le produit est :', Prod)
-
->>>>>>> projetb2
-=======
 Data = [1, 3, 5]
 if Data :
 print('le produit est :', produit(Data))
@@ -21,3 +7,4 @@ print('le max est :', max(Data))
 else :
 print ('dossier vide')
 >>>>>>> projetb2
+
