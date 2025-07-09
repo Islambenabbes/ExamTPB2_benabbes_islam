59af614758ba8bad48087cc7f34310205f0af1f4
Ben Abbes Islam <benabbesislam0@gmail.com>
2025-07-09 20:07:22 +0100

    Fusion squash de projetb1

--- projet.py
+++ projet.py
@@ -12,3 +12,12 @@ Prod = math.prod(Data)
 print ('le produit est :', Prod)
 
 >>>>>>> projetb2
+=======
+Data = [1, 3, 5]
+if Data :
+print('le produit est :', produit(Data))
+print('le min est :', min(Data))
+print('le max est :', max(Data))
+else :
+print ('dossier vide')
+>>>>>>> projetb2
