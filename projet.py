e5b119148e6f643c5acb32972160b9aec43c8648
Ben Abbes Islam <benabbesislam0@gmail.com>
2025-07-09 20:22:34 +0100

    Squash merge projetb2 - résolution des conflits

--- projet.py
+++ projet.py
@@ -1,13 +1,19 @@
 Data = [1, 3, 5]
+<<<<<<< HEAD
 print('le produit est :', produit(Data))
 print('le min est :', min(Data))
 print('le max est :', max(Data))
 
 Data = [1, 3, 5]
+=======
+>>>>>>> projetb2
 if Data :
 print('le produit est :', produit(Data))
 print('le min est :', min(Data))
 print('le max est :', max(Data))
 else :
 print ('dossier vide')
+<<<<<<< HEAD
 
+=======
+>>>>>>> projetb2
